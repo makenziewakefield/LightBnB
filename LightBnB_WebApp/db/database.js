25eea9755b5d84b2bc5a88e36b592a34efa79e26
makenziewakefield <wakefieldmakenzie@gmail.com>
2024-06-10 14:31:05 -0600

    Refractor getAllReservations function to fetch data from postgres

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -94,7 +94,20 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+    SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+    `;
+    return pool
+      .query(queryString, [guest_id, limit])
+      .then(result => result.rows)
+      .catch(err => console.log(err.message));
 };
 
 /// Properties
